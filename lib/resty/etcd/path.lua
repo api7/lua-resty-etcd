ed626877994fe53d4bb52b0820034fd084a4e209
Yuansheng <membphis@gmail.com>
2019-05-08 02:19:15 -0400

    change: code style.

--- lib/resty/etcd/path.lua
+++ lib/resty/etcd/path.lua
@@ -1,64 +1,56 @@
 local clear_tab = require "table.clear"
+local split = require "ngx.re" .split
 local concat_tab = table.concat
 local tostring = tostring
 local select = select
-local split = require "ngx.re" .split
 local ipairs = ipairs
 
 
-local _M = {}
-
-local normalize
-do
-    local res = {}
+    local items = {}
 local function concat(sep, ...)
     local argc = select('#', ...)
-    clear_tab(res)
+    clear_tab(items)
     local len = 0
 
     for i = 1, argc do
         local v = select(i, ...)
         if v ~= nil then
             len = len + 1
-            res[len] = tostring(v)
+            items[len] = tostring(v)
         end
     end
 
-    return concat_tab(res, sep);
+    return concat_tab(items, sep);
 end
 
+
     local segs = {}
-function _M.normalize(...)
+return function (...)
     local path = concat('/', ...)
     -- ngx.log(ngx.WARN, "path: ", path)
-    local res = {}
-    local len = 0
+    local names = {}
+    local err
 
-    local segs, err = split(path, [[/]], "jo", nil, nil, segs)
+    segs, err = split(path, [[/]], "jo", nil, nil, segs)
     if not segs then
         return nil, err
     end
-    -- ngx.log(ngx.WARN, "segs: ", require("cjson").encode(segs))
 
-    for i,seg in ipairs(segs) do
+    local len = 0
+    for _, seg in ipairs(segs) do
         if seg == '..' then
             if len > 0 then
                 len = len - 1
             end
-        
-        elseif seg == '' or seg == '/' and res[len] == '/' then
+
+        elseif seg == '' or seg == '/' and names[len] == '/' then
             -- do nothing
 
         elseif seg ~= '.' then
             len = len + 1
-            res[len] = seg
+            names[len] = seg
         end
     end
 
-    return '/' .. concat_tab(res, '/', 1, len);
+    return '/' .. concat_tab(names, '/', 1, len);
 end
-
-end -- do
-
-
-return _M
