4e2f21f140e527696c48e38ca1a602ab71575644
DoubleSpout <53822985@qq.com>
2019-11-07 09:30:14 +0800

    grant role

--- t/v3/add-auth.sh
+++ t/v3/add-auth.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-etcdctl --endpoints="${AUTH_ENDPOINT_V3}" role add root
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" user add "${AUTH_USER}:${AUTH_PWD}"
-etcdctl --endpoints="${AUTH_ENDPOINT_V3}" grant-role root
+etcdctl --endpoints="${AUTH_ENDPOINT_V3}" role add root
+etcdctl --endpoints="${AUTH_ENDPOINT_V3}" user grant-role root root
+etcdctl --endpoints="${AUTH_ENDPOINT_V3}" role list
+etcdctl --endpoints="${AUTH_ENDPOINT_V3}" user list
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" auth enable
