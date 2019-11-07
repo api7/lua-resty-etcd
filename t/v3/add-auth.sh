be3be94808fc18e6160e7dfa159cf29c5769b2ff
DoubleSpout <53822985@qq.com>
2019-11-07 09:17:44 +0800

    2

--- t/v3/add-auth.sh
+++ t/v3/add-auth.sh
@@ -2,4 +2,4 @@
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" role add root
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" user add "${AUTH_USER}:${AUTH_PWD}"
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" grant-role root
-etcdctl --user="${AUTH_USER}:${AUTH_PWD}" --endpoints="${AUTH_ENDPOINT_V3}" auth enable
+etcdctl --endpoints="${AUTH_ENDPOINT_V3}" auth enable
