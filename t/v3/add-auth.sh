cd2b34df23b79cf7bab085451cdaa9cd4d78797e
DoubleSpout <53822985@qq.com>
2019-11-07 09:34:02 +0800

    export ETCDCTL_API=3

--- t/v3/add-auth.sh
+++ t/v3/add-auth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export ETCDCTL_API=3 
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" user add "${AUTH_USER}:${AUTH_PWD}"
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" role add root
 etcdctl --endpoints="${AUTH_ENDPOINT_V3}" user grant-role root root
