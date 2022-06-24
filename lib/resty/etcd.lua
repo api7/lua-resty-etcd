26406eb294371f594bf38195fb152049f51c92e8
tzssangglass <tzssangglass@gmail.com>
2022-06-24 15:23:43 +0800

    rm fetch version

--- lib/resty/etcd.lua
+++ lib/resty/etcd.lua
@@ -1,86 +1,10 @@
 local etcdv3  = require("resty.etcd.v3")
-local utils   = require("resty.etcd.utils")
 local typeof  = require("typeof")
 local require = require
 local pcall   = pcall
-local decode_json = require("cjson.safe").decode
-local http = require("resty.http")
-local ipairs = ipairs
-local prefix_v3 = {
-    ["3.5."] = "/v3",
-    ["3.4."] = "/v3",
-    ["3.3."] = "/v3beta",
-    ["3.2."] = "/v3alpha",
-}
 
 local _M = {version = 0.9}
 
-
-local function fetch_version(host, timeout, ssl_verify)
-    local uri = host .. "/version"
-    local http_cli, err = http.new()
-    if err then
-        return nil, err
-    end
-
-    if timeout then
-        http_cli:set_timeout(timeout * 1000)
-    end
-
-    local headers = {
-        ['Content-Type'] = "application/x-www-form-urlencoded",
-    }
-
-    local res
-    res, err = http_cli:request_uri(uri, {
-        method = 'GET',
-        headers = headers,
-        ssl_verify = ssl_verify,
-    })
-
-    if err then
-        return nil, err
-    end
-
-    if res.status >= 500 then
-        return nil, "invalid response code: " .. res.status
-    end
-
-    if res.status == 401 then
-        return nil, "insufficient credentials code: " .. res.status
-    end
-
-    if not typeof.string(res.body) then
-        return res
-    end
-
-    res.body = decode_json(res.body)
-    return res.body
-end
-
-local function etcd_version(opts)
-    if typeof.string(opts.http_host) then
-        return fetch_version(opts.http_host, opts.timeout, opts.ssl_verify)
-    end
-
-    if typeof.table(opts.http_host) and #opts.http_host == 1 then
-        return fetch_version(opts.http_host[1], opts.timeout, opts.ssl_verify)
-    end
-
-    if typeof.table(opts.http_host) and #opts.http_host > 1 then
-        local err
-        for _, host in ipairs(opts.http_host) do
-            local ver
-            ver, err = fetch_version(host, opts.timeout, opts.ssl_verify)
-            if ver then
-                return ver
-            end
-        end
-        return nil, err
-    end
-
-    return nil, "invalid etcd host format"
-end
 
 local function require_serializer(serializer_name)
     if serializer_name then
@@ -110,15 +34,7 @@ function _M.new(opts)
 
     local serializer_name = typeof.string(opts.serializer) and opts.serializer
     opts.serializer = require_serializer(serializer_name)
-
-    if not opts.api_prefix or not utils.has_value(prefix_v3, opts.api_prefix) then
-        local ver, err = etcd_version(opts)
-        if not ver then
-            return nil, err
-        end
-        local sub_ver = ver.etcdserver:sub(1, 4)
-        opts.api_prefix = prefix_v3[sub_ver] or "/v3beta"
-    end
+    opts.api_prefix = "/v3"
 
     return etcdv3.new(opts)
 end
--- lib/resty/etcd/v3.lua
+++ lib/resty/etcd/v3.lua
@@ -118,7 +118,7 @@ local function http_request_uri(self, http_cli, method, uri, body, headers, keep
     end
 
     local full_uri
-    if utils.starts_with(uri, "/version") or utils.starts_with(uri, "/v2") then
+    if utils.starts_with(uri, "/version") then
         full_uri = endpoint.http_host .. uri
     else
         full_uri = endpoint.full_prefix .. uri
